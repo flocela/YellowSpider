86fc6d40d544063c070d1628e91ea49614238cc9
Flo Maldonado <flo@Aureas-MacBook-Pro.local>
2024-12-31 20:26:54 -0800

    Should have been part of last commit, where leg was added to spider body.

--- YellowSpider/Leg.hpp
+++ YellowSpider/Leg.hpp
@@ -1,13 +1,30 @@
-//
-//  Leg.hpp
-//  YellowSpider
-//
-//  Created by Flo Maldonado on 12/31/24.
-//
+#include <glm/glm.hpp>
+#include <vector>
+#include "Utilities.hpp"
 
 #ifndef Leg_hpp
 #define Leg_hpp
 
-#include <stdio.h>
+class Leg
+{
+    public:
+    
+    Leg() = delete;
+    Leg(float seg0, float seg1, float seg2);
+    Leg(const Leg& o) = default;
+    Leg(Leg&& o) noexcept = default;
+    Leg& operator= (const Leg& o) = default;
+    Leg& operator= (Leg&& o) noexcept = default;
+    ~Leg() = default;
+    
+    std::vector<glm::mat4> getModels();
+    
+    private:
+    
+    float _seg0Length = 0.0;
+    float _seg1Length = 0.0;
+    float _seg2Length = 0.0;
+
+};
 
 #endif /* Leg_hpp */
